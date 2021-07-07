fb5d795b405d163f51164189200cacceef656368
Gerhard <g.gehrer@htl-leonding.ac.at>
2021-07-07 22:51:00 +0200

    Added project SolutionDeleter

---
+++ SolutionDeleter.ConApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace SolutionDeleter.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutionPath = GetCurrentSolutionPath();
+
+            Console.WriteLine(nameof(SolutionDeleter));
+            RunProgress();
+
+            CSharpCodeGenerator.Logic.Generator.DeleteGenerationFiles(solutionPath);
+        }
+
+        private static void RunProgress()
+        {
+            var t = new Thread(() =>
+            {
+                bool hpos = true;
+                int top = Console.CursorTop + 1;
+
+                while (true)
+                {
+                    Console.SetCursorPosition(0, top);
+                    if (hpos)
+                    {
+                        Console.Write("---");
+                    }
+                    else
+                    {
+                        Console.Write(" | ");
+                    }
+                    hpos = !hpos;
+                    Thread.Sleep(500);
+                }
+            })
+            {
+                IsBackground = true
+            };
+            t.Start();
+        }
+        private static string GetCurrentSolutionPath()
+        {
+            int endPos = AppContext.BaseDirectory
+                                   .IndexOf($"{nameof(SolutionDeleter)}", StringComparison.CurrentCultureIgnoreCase);
+
+            return AppContext.BaseDirectory.Substring(0, endPos);
+        }
+
+    }
+}
