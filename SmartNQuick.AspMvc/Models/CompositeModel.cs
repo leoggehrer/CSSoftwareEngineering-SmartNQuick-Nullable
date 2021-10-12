490746c027c21ed80aa64ef0793458283fda8be5
Gerhard <g.gehrer@htl-leonding.ac.at>
2021-10-12 15:47:20 +0200

    Added CompositeModel

---
+++ SmartNQuick.AspMvc/Models/CompositeModel.cs
@@ -0,0 +1,45 @@
+﻿//@BaseCode
+//MdStart
+
+namespace SmartNQuick.AspMvc.Models
+{
+    public abstract partial class CompositeModel<TConnector, TConnectorModel, TOne, TOneModel, TAnother, TAnotherModel> : IdentityModel
+        where TConnector : Contracts.IIdentifiable
+        where TOne : Contracts.IIdentifiable
+        where TAnother : Contracts.IIdentifiable
+        where TConnectorModel : IdentityModel, Contracts.ICopyable<TConnector>, TConnector, new()
+        where TOneModel : IdentityModel, Contracts.ICopyable<TOne>, TOne, new()
+        where TAnotherModel : IdentityModel, Contracts.ICopyable<TAnother>, TAnother, new()
+    {
+        public virtual TConnectorModel ConnectorModel { get; } = new TConnectorModel();
+        public virtual TConnector ConnectorItem => ConnectorModel;
+
+        public virtual TOneModel OneModel { get; } = new TOneModel();
+        public virtual TOne OneItem => OneModel;
+        public bool OneItemIncludeSave { get; set; }
+
+        public virtual TAnotherModel AnotherModel { get; } = new TAnotherModel();
+        public virtual TAnother AnotherItem => AnotherModel;
+        public bool AnotherItemIncludeSave { get; set; }
+
+        public override int Id { get => ConnectorModel.Id; set => ConnectorModel.Id = value; }
+        public byte[] RowVersion
+        {
+            get
+            {
+                var result = default(byte[]);
+
+                if (ConnectorModel is VersionModel ve)
+                    result = ve.RowVersion;
+
+                return result;
+            }
+            set
+            {
+                if (ConnectorModel is VersionModel ve)
+                    ve.RowVersion = value;
+            }
+        }
+    }
+}
+//MdEnd
