77ecebdcfff1b29beda317d7ba1312c0c937e412
Gerhard Gehrer <g.gehrer@hotmail.com>
2021-10-18 13:35:12 +0200

    Added Translator

---
+++ SmartNQuick.AspMvc/Modules/Language/Translator.cs
@@ -0,0 +1,33 @@
+﻿//@BaseCode
+//MdStart
+
+namespace SmartNQuick.AspMvc.Modules.Language
+{
+	public partial class Translator
+	{
+        static Translator()
+        {
+            ClassConstructing();
+            ClassConstructed();
+        }
+        static partial void ClassConstructing();
+        static partial void ClassConstructed();
+
+        private Translator()
+        {
+            Constructing();
+            Constructed();
+        }
+        partial void Constructing();
+        partial void Constructed();
+        protected static Translator instance = null;
+        public static Translator Instance => instance ??= new Translator();
+
+		protected virtual string Translate(string key) => key;
+        protected virtual string Translate(string key, string defaultValue) => defaultValue;
+
+        public static string TranslateIt(string key) => Instance.Translate(key);
+        public static string TranslateIt(string key, string defaultValue) => Instance.Translate(key);
+    }
+}
+//MdEnd
