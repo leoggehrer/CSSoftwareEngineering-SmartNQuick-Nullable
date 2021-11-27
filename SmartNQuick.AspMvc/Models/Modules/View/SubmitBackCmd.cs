ca232e0d2f2235cd54dd875e9e7570daf3c568e8
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2021-11-27 01:04:58 +0100

    Added _CardItem for more customizing.

--- SmartNQuick.AspMvc/Models/Modules/View/SubmitBackCmd.cs
+++ SmartNQuick.AspMvc/Models/Modules/View/SubmitBackCmd.cs
@@ -1,6 +1,5 @@
 //@BaseCode
 //MdStart
-
 namespace SmartNQuick.AspMvc.Models.Modules.View
 {
     public partial class SubmitBackCmd : SubmitCmd
@@ -16,6 +15,7 @@ namespace SmartNQuick.AspMvc.Models.Modules.View
         public bool FromSubmitToBack { get; set; } = false;
         public string BackText { get; set; } = "Back to List";
         public string BackAction { get; set; } = "Index";
+        public object BackRouteValues { get; set; }
         public string BackController { get; set; }
         public string BackCss { get; set; } = "btn btn-outline-dark";
         public string BackStyle { get; set; } = "min-width: 8em;";
